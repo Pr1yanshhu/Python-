c767edf2d260fea9a219b06c07ea5459bf445ef9
Priyanshu Kumar <priyanshusg24@gmail.com>
2026-07-05 01:28:58 +0530

    Create Practise.py

---
+++ Practise.py
@@ -0,0 +1,8 @@
+print("Myself Priyanshu")
+name = input("What is Your name? ")
+print("Hello, " + name)
+# baby steps!
+city = input("Which City do you live in? ")
+print("Hello, " + name + " from " + city ) 
+
+
