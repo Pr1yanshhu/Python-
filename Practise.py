5afa0877dcb8378f59224e8c98b7a56513a2ce88
Priyanshu Kumar <priyanshusg24@gmail.com>
2026-07-10 00:44:08 +0530

    Update Practise.py

--- Practise.py
+++ Practise.py
@@ -1,11 +1,2 @@
-
-name = input("What is Your name? ")
-print("Hello, " + name)
-# baby steps!
-city = input("Which City do you live in? ")
-print("Hello, " + name + " from " + city ) 
-age = int(input("What is your Birthyear? "))
-print("You'll be " + str(2030 - age) + " in 2030")
-print("Thank You")
-# Have to watch Lecture 1 again....
-#havent done practise
+name = input("What is your name? ")
+print("Hello," , name)
